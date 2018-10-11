79508247925d1f1b13081a53b164150121990316
Eric Liang <ewliang@users.noreply.github.com>
2018-10-11 08:05:37 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById('canvas');
+canvas.width = 1000;
+canvas.height = 1000;
+if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+  var background = new Image();
+background.src = "https://i.pinimg.com/originals/1b/70/61/1b7061e57ff4edde0b2d1628e508b6c4.jpg";
+  background.onload = function(){
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.font = '24px Open Sans';
+    ctx.fillStyle = 'Black';
+    var text = "Hello World";
+    var textWidth = ctx.measureText(text).width;
+    ctx.fillText(text, (canvas.width/2) - (textWidth/2), canvas.height/2);
+  }
+}

af88329098f9d1229e6ade2ab2ed00760adea71d
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-11 10:14:46 -0700

    Updated font and added stroke style text.

--- scripts.js
+++ scripts.js
@@ -13,12 +13,16 @@ if (canvas.getContext) {
   background.src = "https://i.pinimg.com/originals/1b/70/61/1b7061e57ff4edde0b2d1628e508b6c4.jpg";
   background.onload = function(){
     submitButton.onclick = function() {
-      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      ctx.font = '24px Open Sans';
-      ctx.fillStyle = 'Black';
+      ctx.drawImage(background, 0, 0, canvas.width, canvas.height); // Draw Background Image
+      // Text Styles
+      ctx.font = '24px Impact';
+      ctx.fillStyle = 'White';
+      ctx.strokeStyle = 'Black';
       text = textInput.value;
       var textWidth = ctx.measureText(text).width;
-      ctx.fillText(text, (canvas.width/2) - (textWidth/2), canvas.height/2);
+      // Draw Text
+      ctx.fillText(text, (canvas.width/2) - (textWidth/2), canvas.height/2); // Solid Text
+      ctx.strokeText(text, (canvas.width/2) - (textWidth/2), canvas.height/2); // Give Solid Text a Stroke Style
     }
   }
 }

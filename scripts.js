8774789dc34a86cd6e8f114e44928e49d01069fa
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-11 09:51:25 -0700

    Added support for custom text input via input field.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
 var canvas = document.getElementById('canvas');
+var textInput = document.getElementById('textInput');
+var submitButton = document.getElementById('submitButton');
+
+var text = '';
+
 canvas.width = 500;
 canvas.height = 500;
+
 if (canvas.getContext) {
   var ctx = canvas.getContext('2d');
   var background = new Image();
-background.src = "https://i.pinimg.com/originals/1b/70/61/1b7061e57ff4edde0b2d1628e508b6c4.jpg";
+  background.src = "https://i.pinimg.com/originals/1b/70/61/1b7061e57ff4edde0b2d1628e508b6c4.jpg";
   background.onload = function(){
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.font = '24px Open Sans';
-    ctx.fillStyle = 'Black';
-    var text = "Hello World";
-    var textWidth = ctx.measureText(text).width;
-    ctx.fillText(text, (canvas.width/2) - (textWidth/2), canvas.height/2);
+    submitButton.onclick = function() {
+      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      ctx.font = '24px Open Sans';
+      ctx.fillStyle = 'Black';
+      text = textInput.value;
+      var textWidth = ctx.measureText(text).width;
+      ctx.fillText(text, (canvas.width/2) - (textWidth/2), canvas.height/2);
+    }
   }
 }
